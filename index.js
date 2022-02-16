73ff53052ee50995e9f8077c9ba43c11322684dc
IuriSampaio <iurisampaio18@gmail.com>
2022-02-16 12:51:14 -0300

    add logs

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post( '/' ,parser.single('file'), async ( req , res ) => {
     `;
     console.log(command);
     exec(command, function (error, stdout, stderr) {
-        console.table(error, stdout, stderr);
+        console.log(error, stdout, stderr);
         fs.unlink(req.file.path, function(e){
             if (e != null) console.error("ERRO AO DELETAR O ANEXO DO SERVIDOR "+e)
         });
