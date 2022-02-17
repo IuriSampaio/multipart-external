3c32362617602589ab06cdf333b74f3d9df8208e
IuriSampaio <iurisampaio18@gmail.com>
2022-02-16 21:57:17 -0300

    testes

--- index.js
+++ index.js
@@ -34,49 +34,29 @@ app.get( '/' , (req,res)=>{
 });
 
 
-// Accept: */*
-// Accept-Encoding: gzip, deflate, br
-// Accept-Language: pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6
-// Access-Control-Request-Headers: content-type,x-usertoken
-// Access-Control-Request-Method: POST
-// Connection: keep-alive
-// Host: demo-form-external-unicred.herokuapp.com
-// Origin: https://unicreddev.service-now.com
-// Sec-Fetch-Dest: empty
-// Sec-Fetch-Mode: cors
-// Sec-Fetch-Site: cross-site
-// User-Agent:
-
-
 app.post( '/' ,parser.single('file'), async ( req , res ) => {
 
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, x-usertoken , Accept-Encoding, Connection, Host, Sec-Fetch-Dest, Sec-Fetch-Mode, User-Agent')
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+    const command = `curl "${req.query.url}" \
+        --request POST \
+        --header Accept:application/json \
+        --user ${req.query.user_name}:${req.query.password} \
+        --header Content-Type:multipart/form-data \
+        -F table_name=${req.body.table_name} \
+        -F table_sys_id=${req.body.table_sys_id} \
+        -F uploadFile=@${req.file.path}
+    `;
+    
+    exec(command, function (error, stdout, stderr) {
+        console.log(error, stdout, stderr);
+        fs.unlink(req.file.path, function(e){
+            if (e != null) console.error("ERRO AO DELETAR O ANEXO DO SERVIDOR "+e)
+        });
 
-    // const command = `curl "${req.query.url}" \
-    //     --request POST \
-    //     --header Accept:application/json \
-    //     --user ${req.query.user_name}:${req.query.password} \
-    //     --header Content-Type:multipart/form-data \
-    //     -F table_name=${req.body.table_name} \
-    //     -F table_sys_id=${req.body.table_sys_id} \
-    //     -F uploadFile=@${req.file.path}
-    // `;
-    var body = Object.assign({}, req.body)
-    console.log({file: req.file, body, req:req});
-    // exec(command, function (error, stdout, stderr) {
-    //     console.log(error, stdout, stderr);
-    //     fs.unlink(req.file.path, function(e){
-    //         if (e != null) console.error("ERRO AO DELETAR O ANEXO DO SERVIDOR "+e)
-    //     });
-
-    //     res.send(stdout);
+        res.send(stdout);
         
-    //     if (error !== null) 
-    //         res.status(400).send(stderr);
-    // });
-    res.send({teste: 'req'});
+        if (error !== null) 
+            res.status(400).send(stderr);
+    });
 });
 
 
